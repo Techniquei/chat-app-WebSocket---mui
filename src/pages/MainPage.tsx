a0e7d6ddf274c00bd765e0e95903228dc4b0598a
Techniquei <66029763+Techniquei@users.noreply.github.com>
2023-03-30 19:18:03 +0300

    quickFix

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -13,12 +13,15 @@ import {
   Typography,
 } from "@mui/material"
 import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
 import { userAPI, UserItem } from "../api/axios"
 import { Photos } from "../store"
 
 export function MainPage() {
   const [usersState, setUsersState] = useState<UserItem[]>([])
   const [totalPageState, setTotalPageState] = useState<number>(1)
+  const [loadingState, setLoadingState] = useState(false)
+  const navigate = useNavigate()
   useEffect(() => {
     userAPI.getUsers(1).then((res) => {
       setUsersState(res.data.items)
@@ -26,9 +29,12 @@ export function MainPage() {
     })
   }, [])
   function paginationHandler(page: number) {
-    userAPI
-      .getUsers(page)
-      .then((res) => setUsersState(res.data.items))
+    setLoadingState(true)
+    console.log("true")
+    userAPI.getUsers(page).then((res) => {
+      setUsersState(res.data.items)
+      setLoadingState(false)
+    })
   }
   if (usersState === null) return <CircularProgress sx={{ width: "auto" }} />
   console.log(usersState)
@@ -43,31 +49,42 @@ export function MainPage() {
         mb: 3,
       }}
     >
-      <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap" }}>
-        {usersState.map((user) => (
-          <Card elevation={3} sx={{ width: 132 }}>
-            <CardContent sx={{ p: 0 }}>
-              <Avatar
-                variant="rounded"
-                src={user.photos.small}
-                sx={{ width: "132px", height: "100px", mb: 1 }}
-              />
-              <Typography textAlign={"center"} variant="body1" fontWeight={600}>
-                {user.name}
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing sx={{ justifyContent: "flex-end" }}>
-              <IconButton>
-                <Bookmark />
-              </IconButton>
-              <IconButton>
-                <Message />
-              </IconButton>
-            </CardActions>
-          </Card>
-        ))}
+      <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap", minHeight: 595 }}>
+        {loadingState ? (
+          <CircularProgress sx={{ width: "auto"}} />
+        ) : (
+          usersState.map((user) => (
+            <Card elevation={3} sx={{ width: 132, cursor: "pointer" }} onClick={()=>navigate(`/profile/${user.id}`)} >
+              <CardContent sx={{ p: 0 }}>
+                <Avatar
+                  variant="rounded"
+                  src={user.photos.small}
+                  sx={{ width: "132px", height: "100px", mb: 1 }}
+                />
+                <Typography
+                  textAlign={"center"}
+                  variant="body1"
+                  fontWeight={600}
+                >
+                  {user.name}
+                </Typography>
+              </CardContent>
+              <CardActions disableSpacing sx={{ justifyContent: "flex-end" }}>
+                <IconButton>
+                  <Bookmark />
+                </IconButton>
+                <IconButton>
+                  <Message />
+                </IconButton>
+              </CardActions>
+            </Card>
+          ))
+        )}
       </Box>
-      <Pagination count={totalPageState} onChange={(e, page)=>paginationHandler(page)} />
+      <Pagination
+        count={totalPageState}
+        onChange={(e, page) => paginationHandler(page)}
+      />
     </Container>
   )
 }
